fabf2571cc85ebe8b8644967bc214d24bbe1e562
toast254 <toast254@users.noreply.github.com>
2017-05-31 13:12:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import uuid
 import logging
-from redis import Redis
 from tornado import ioloop, web
 from Handlers.MainHandler import MainHandler
-from Handlers.LoginHandler import LoginHandler
-from Handlers.LogoutHandler import LogoutHandler
+from Tools import PostgreSQL
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filename='website.log',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 
-app_port = '8080'
-redis_host = '127.0.0.1'
-redis_port = '6379'
-redis_db = 0
-redis_password = None
+app_port = '8888'
+if 'OPENSHIFT_PYTHON_PORT' in os.environ:
+    app_port = os.environ['OPENSHIFT_PYTHON_PORT']
 
 
 class Application(web.Application):
 
-    def __init__(self, redis_client: Redis):
+    def __init__(self):
         handlers = [
             (r'/', MainHandler),
-            (r'/login', LoginHandler, dict(redis_client=redis_client)),
-            (r'/logout', LogoutHandler),
         ]
         settings = {
             'cookie_secret': ''.join([str(uuid.uuid4()) for _ in range(8)]),
@@ -37,8 +32,9 @@ class Application(web.Application):
 
 
 def main():
-    redis_client = Redis(host=redis_host, port=redis_port, db=redis_db, password=redis_password)
-    app = Application(redis_client=redis_client)
+    # PostgreSQL.reset_db()
+    PostgreSQL.init_db()
+    app = Application()
     app.listen(port=app_port)
     ioloop.IOLoop.current().start()
 
