f0f20089bc1c68c6547156f4f7b20116a5128dc7
toast254 <toast254@users.noreply.github.com>
2017-05-31 15:12:52 +0200

    Update RegisterHandler.py

--- Handlers/RegisterHandler.py
+++ Handlers/RegisterHandler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from tornado import escape
 from Handlers.BaseHandler import BaseHandler
 from Tools import PostgreSQL, VerifyFields
 
@@ -16,13 +17,25 @@ class RegisterHandler(BaseHandler):
 
     def post(self):
         """Get user completed form and verify it before save it"""
-        prenom = self.get_body_argument('prenom')
-        nom = self.get_body_argument('nom')
-        courriel = self.get_body_argument('courriel')
-        genre = self.get_body_argument('genre')
-        promotion = int(self.get_body_argument('promotion'))
-        if VerifyFields.verify_all(prenom, nom, courriel, genre, promotion):
-            PostgreSQL.insert_inscrit(prenom, nom, genre, courriel, promotion)
-            self.render('registered.html')
-        else:
-            self.send_error(status_code=400)
+        prenom = escape.xhtml_escape(self.get_body_argument('prenom'))[:64]
+        nom = escape.xhtml_escape(self.get_body_argument('nom'))[:64]
+        courriel = escape.xhtml_escape(self.get_body_argument('courriel'))[:96]
+        genre = escape.xhtml_escape(self.get_body_argument('genre'))[:16]
+        promotion = int(escape.xhtml_escape(self.get_body_argument('promotion'))[:4])
+        prenom_accompagnateurs = self.get_body_arguments('accompagnateurs-prenom')
+        nom_accompagnateurs = self.get_body_arguments('accompagnateurs-nom')
+        accompagnateurs = []
+        size = len(prenom_accompagnateurs)
+        if size == len(nom_accompagnateurs):
+            for i in range(0, size):
+                a_prenom = escape.xhtml_escape(prenom_accompagnateurs[i])
+                a_nom = escape.xhtml_escape(nom_accompagnateurs[i])
+                if not a_prenom or not a_nom:
+                    self.send_error(status_code=400)
+                    return
+                accompagnateurs.append((a_prenom, a_nom))
+        if VerifyFields.verify_all(prenom, nom, courriel, genre, promotion, accompagnateurs):
+            if PostgreSQL.insert_inscrit(prenom, nom, genre, courriel, promotion, accompagnateurs):
+                self.render('registered.html')
+                return
+        self.send_error(status_code=400)
