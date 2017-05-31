2e2910d76cd8922ce48cc6c37ac978e4a41b88ba
toast254 <toast254@users.noreply.github.com>
2017-05-31 13:55:57 +0200

    Create VerifyFields.py

---
+++ Tools/VerifyFields.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import re
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+
+EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')
+
+
+def verify_identity(prenom: str, nom: str):
+    return nom and len(nom) > 0 and prenom and len(prenom) > 0
+
+
+def verify_email(courriel: str):
+    return courriel and len(courriel) > 0 and EMAIL_REGEX.match(courriel)
+
+
+def verify_promotion(promotion: int):
+    return promotion and datetime.datetime.now().year <= promotion <= datetime.datetime.now().year + 3
+
+
+def verify_genre(genre: str):
+    return genre in ('personnel', 'professionnel', 'ancien', 'etudiant')
+
+
+def verify_all(prenom: str, nom: str, courriel: str, genre: str, promotion):
+    return verify_identity(prenom, nom) and verify_email(courriel) \
+           and verify_genre(genre) and verify_promotion(promotion)
